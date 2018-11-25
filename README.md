e22a6f3900b67eed344d85ba279d4eeaf0fb16c7
sz1358 <sz1358@nyu.edu>
2018-11-25 04:25:29 -0800

    2.0: setup boost program_options

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Oth
+
+Compile Boost with gcc 8.2 and C++ 2017 standard
+
+Before Run, set path variable
+```
+DYLD_LIBRARY_PATH=/usr/local/boost/lib
+```
--- othello.cpp
+++ othello.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-#include <string>
 #include <boost/program_options.hpp>
-#include "include/game.h"
-using namespace std;
 
-int main(int args, char *argv[]) {
-    int gameType; // 0: player first, 1: opponent first
-    Game game;
-    game.setup(0);
+namespace po = boost::program_options;
+
+int main(int argc, const char* argv[]){
+    po::options_description description("Othello Usage");
     return 0;
 }
